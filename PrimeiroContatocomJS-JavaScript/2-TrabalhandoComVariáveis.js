6c9999dfc48ef2e3bbd907db688898c8fc7940ee
Carlos Henrique Nunes <117055427+CrMessiProgrammer@users.noreply.github.com>
2023-08-03 23:24:14 -0300

    Initial commit

---
+++ PrimeiroContatocomJS-JavaScript/2-TrabalhandoComVariáveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS (JavaScript) é case sensitive
+
+const idade = 20;
+const nome = "Ricardo";
+
+//não fazer
+//mes = "Janeiro";
---
+++ PrimeiroContatocomJS-JavaScript/3-OperaçõesAritméticas.js
@@ -0,0 +1,11 @@
+console.log("Operações Aritméticas");
+
+console.log(2 + 2);
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+
+console.log("1" + 2 + 4);
+console.log(5 + 4 + "3");
